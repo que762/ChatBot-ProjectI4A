0faa21f6346bb9fb26d5eb531088e6ee74c1aa8c
que762 <qnge@duck.com>
2024-06-09 23:01:35 +0200

    debug

--- utils/formation_dataset.py
+++ utils/formation_dataset.py
@@ -2,16 +2,18 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-formations_dataset = pd.read_csv('formations_description.csv')
+formations_dataset = pd.read_csv('utils/formations_description.csv')
+formations_dataset.drop_duplicates(subset=['description'], inplace=True)
 print("Loading model...")
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 print("Model loaded")
 print("Loading embeddings...")
-embeddings = pd.read_csv('formations_embeddings.csv').values
+embeddings = pd.read_csv('utils/formations_embeddings.csv').values
 print("Embeddings loaded")
+print(embeddings)
 
 
-def compare_to_each_row(sentence, embeddings):
+def compare_to_each_row(sentence):
     """Compare a sentence to each sentence in the dataset and return the similarities."""
     sentence_embedding = model.encode(sentence)
     similarities = cosine_similarity([sentence_embedding], embeddings)
@@ -20,6 +22,7 @@ def compare_to_each_row(sentence, embeddings):
 
 def sort_by_most_similar(similarities):
     """Sort the dataset by the most similar formations."""
+    # add the similarity column
     formations_dataset['similarity'] = similarities[0]
     if max(similarities[0]) < 0.3: # if not similar enough
         return None
