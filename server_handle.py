db7d7d750501db1de1c623af0fb252e4eb21798e
que762 <qnge@duck.com>
2024-06-13 10:51:34 +0200

    douille

--- server_handle.py
+++ server_handle.py
@@ -4,10 +4,26 @@ import logging
 import yaml
 
 import pipeline
-#import vigogne
+import rabbit_functions as RabbitMq
+
+def process_messages():
+    while True:
+        request = RabbitMq.get_last_request(queue_name='messages_users')
+        if request:
+            user_id = request['user_id']
+            user_message = request['message']
+            
+            # Process the message with your AI
+            response_message = pipeline.educhat(user_id, user_message)
+            
+            # Send the response back to the queue messages_IA
+            response = {'user_id': user_id, 'message': response_message}
+            RabbitMq.send_result(response, queue_name='messages_IA')
 
 server = Flask(__name__)
 server.config['SECRET_KEY'] = 'edubotkey'
+# no timeout
+server.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 socketio = SocketIO(server, cors_allowed_origins="*")
 
 # Logging
@@ -30,6 +46,18 @@ def disconnect(msg):
 @socketio.on('user_message')
 def handle_message(msg):
 	logger.debug('User message: ' + msg['message'])
-	response = pipeline.educhat(msg['user_id'], msg['message'])
-	#response, _ = vigogne.chat(msg['message'])
+	user_id = msg['user_id']
+	user_message = msg['message']
+
+    # Envoyer le message à la queue messages_users
+	RabbitMq.send_result({'user_id': user_id, 'message': user_message}, queue_name='messages_users')
+	
+	process_messages()
+
+    # Attendre et récupérer la réponse de la queue messages_IA
+	response = None
+	while response is None:
+		response = RabbitMq.get_last_request(queue_name='messages_IA')
+
+    # Envoyer la réponse à l'utilisateur
 	socketio.emit('ai_message', response)
